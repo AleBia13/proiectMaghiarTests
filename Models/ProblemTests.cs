d66f3dbd46e404a1076e06ec4efe2ea77616e662
AleBia13 <94802812+AleBia13@users.noreply.github.com>
2022-11-02 01:04:08 +0200

    Initial commit

---
+++ Models/ProblemTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using proiectMaghiar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proiectMaghiar.Models.Tests
+{
+    [TestClass()]
+    public class ControllerTest
+    {
+        [TestMethod()]
+        public void TestExceptionCase()
+        {
+            Problem p = new Problem("test");
+            try
+            {
+                p.lengthOfLongestSubstring(p.inputString);
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "Wrong Input");
+                return;
+            }
+            //Assert.Fail();
+        }
+    }
+}
